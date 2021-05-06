23410607e284dff5ff420507f1adcece5eeac754
Miner3210 <patsakorn.mark@mail.kmutt.ac.th>
2021-05-06 13:21:16 +0700

    [ADD] Sort List Desktop

---
+++ src/components/SortBar.vue
@@ -0,0 +1,45 @@
+<template>
+        <div class="p-2">
+        <div class="justify-self-center hidden sm:block">
+            <p class="text-2xl">Sort By</p>
+            <ul class="pt-4">
+                <li @click="wtf('ProductName')" class="mt-2 py-4 hover:border-black border-b-2">ProductName</li>
+                <li @click="wtf('ProductPrice')" class="mt-2 py-4 hover:border-black border-b-2">ProductPrice</li>
+                <li @click="wtf('Manufactureddate')" class="mt-2 py-4 hover:border-black border-b-2 ">Manufactureddate</li>
+            </ul>
+        </div>
+        <div class="sm:hidden">
+            <p class="  text-2xl">Sort By</p>
+            <div class="flex items-end">
+            <select class=" p-0" name="sortValue" id="sort" >
+                  <option value="ProductName">ProductName</option>
+                  <option value="ProductPrice">ProductPrice</option>
+                  <option value="Manufactureddate">Manufactureddate</option>
+            </select>
+            </div>
+        </div>
+        </div>
+</template>
+
+<script>
+
+export default{
+    name:'SortBar',
+    emits:['sort-page']
+    ,methods:{
+        wtf(path){
+            switch(path){
+                case 'ProductName':
+                    this.$emit('sort-page','sortBy=productName')
+                    break;
+                case 'ProductPrice':
+                    this.$emit('sort-page','sortBy=productPrice')   
+                    break;
+                case 'Manufactureddate':
+                    this.$emit('sort-page','sortBy=productManufactureddate') 
+                    break;
+            }
+        }
+    }
+}
+</script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,33 +1,37 @@
 <template>
     <div class="mainlist font-custom">
         <h1 class="text-4xl border-b-2 border-black p-2">New Arrivals</h1>
-        <div class="grid md:grid-cols-5 md:gap-10 m-10 ">
-            <!-- <div v-for="(item,index) in imageTest" :key="`imageTest-${index}`">
-                <img :src="item"/>
-            </div> -->
-            <div  v-for="(item,index) in info"  :key="item.productId">
+        <div class="grid grid-cols-1 md:grid-cols-5 md:gap-10 m-10 ">
+            <div class="col-start-2 col-span-4 md:col-start-1 md:col-auto">
+            <SortBar @sort-page="setSortFillter"></SortBar>
+            </div>
+            <div class="grid md:grid-cols-4 col-span-4 col-start-2 md:gap-10">
+            <div class="py-1" v-for="(item,index) in info"  :key="item.productId">
                 <router-link :to="{ name: 'Product', params: { getproductId: item.productId } }">
-                <img :src="imageTest[index]"  width="500" height="400" class="mx-auto shadow-lg w-60 h-60"/>
+                <img :src="imageTest[index]" class="mx-auto shadow-lg sm:w-45 sm:h-45"/>
                 <p class="pt-2">{{ item.productName }}</p>
                 <p>${{ item.productPrice}}</p>
                 </router-link>
-            <button ><img src="../assets/icon/color-pencil.png" width="40" height="40"></button>
-            <button @click="deleteRoom(item.productId)"><img src="../assets/icon/trash.png" width="45" height="45"></button>
+            <button ><img src="../assets/icon/color-pencil.png" width="35" height="35"></button>
+            <button @click="deleteRoom(item.productId)"><img src="../assets/icon/trash.png" width="35" height="35"></button>
             </div>
         </div>
-
+        </div>
     </div>
     <router-view></router-view>
 </template>
 
 <script>
 const axios = require("axios");
+import SortBar from '../components/SortBar.vue';
 export default {
+  components: { SortBar },
     data(){
         return{
             info:[],
             ImageP:null,
-            imageTest:[]
+            imageTest:[],
+            sortFillter:[]
            
     }},
   name: 'Main',
@@ -39,20 +43,37 @@ export default {
             this.info = this.info.filter( info => info.id !== id );
         },
         async getImage(){
-            
+                this.imageTest =[]
                 const itemmap = this.info.map((number) => { return number.productId})
                 for (let index = 0; index < itemmap.length; index++) {
                 const imageresponse = await axios.get(`http://localhost:8081/image/get/${itemmap[index]}`, {responseType: 'arraybuffer'}).then(response => new Buffer(response.data, 'binary').toString('base64'))
                 const dataImage = `data:image/png;base64,${imageresponse}`
                 this.imageTest.push(dataImage) 
                 }
+        },
+        setSortFillter(path){
+            this.sortFillter = path
+            this.getDataSort()
+            this.getImage()
+            console.log(this.sortFillter)
+        },
+        async getDataSort(){
+            try {
+                const response = await axios.get(`http://localhost:8081/api/products/all?${this.sortFillter}`)
+                this.info = response.data
+                console.log(this.sortFillter)
+                this.getImage()
+                } catch (e) {
+                         console.log(e)
+                }
         }
   }
   ,
   async created() {
     try {
-      const response = await axios.get(`http://localhost:8081/api/products/all`)
+      const response = await axios.get(`http://localhost:8081/api/products/all?${this.sortFillter}`)
       this.info = response.data
+      console.log(this.sortFillter)
     this.getImage()
     } catch (e) {
         console.log(e)
